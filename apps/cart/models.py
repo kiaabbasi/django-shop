b7d6281cb3ec3388306633fad081f8e874ea5d0a
kia <behkia007@gmail.com>
2025-10-26 23:32:51 +0330

    Cart App is added with Cart and CartItem model

---
+++ apps/cart/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from apps.products.models import Product
+from django.contrib.auth.models import User
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)    
+    added_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    discription = models.TextField(null=True, blank=True)
+    on_cart = models.ForeignKey('Cart', on_delete=models.CASCADE, related_name='items')
+    
+    def __str__(self):
+        return f"{self.quantity} of {self.product.name} for {self.on_cart.user.username}"
+    
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f"Cart of {self.user.username}"
